af5b01f6ecee9b12d40767185809b0846f9617d8
Ahan Sardar <ahansardarvis@gmail.com>
2026-04-26 22:24:20 +0530

    updated tv-config

--- tv-config.js
+++ tv-config.js
@@ -1,3 +1,3 @@
 window.TV_APP_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbwnL1FT5W0SEwVqSJ-zyghKZS9mYna4cDakcP245mfPfu12KYd1B_jJ4z6_yQ6NRAMYQA/exec";
+  "https://script.google.com/macros/s/AKfycbwr9UV8Lwlp63s0Q3qiysWCXpYm2IvIgUaq8Qoy7oExyHHf8cH_Vab1MPIWdIMOYMB81A/exec";
 //v5
